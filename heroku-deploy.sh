76f04c25e8d603df45ae6552c8c16a8993ea2ae8
Jemal Mohammed <Jemal.Mohammed@logianalytics.com>
2020-10-26 02:26:07 -0400

    updated node and yarn versions

--- heroku-deploy.sh
+++ heroku-deploy.sh
@@ -5,5 +5,7 @@ set -e
 
 # change the versions in package.json and build.gradle and release before running.
 ./gradlew clean -Pprod bootWar -x test
+# Install java plugin before deploy
+heroku plugins:install java
 #heroku deploy:jar --jar build/libs/*.war --includes newrelic.jar:newrelic.yml
 heroku deploy:jar  build/libs/*.war --app health-by-points-yep
